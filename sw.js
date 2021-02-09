c6cc79dabe5925389b5448424bc8d8f909c5aaf1
xdemocle <xdemocle@gmail.com>
2021-02-09 14:21:01 +0200

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-59bace05c33077bd4c0a.js"
+    "url": "webpack-runtime-1695a3d717446d39af07.js"
   },
   {
     "url": "framework-3e6dec6139dadaa7d0c7.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "3484bde638f27c34b0da253c8ae9f57f"
+    "revision": "84c2694ce199ce9ecd1acf72740e3901"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-19245c8506e49b502b12.js"
